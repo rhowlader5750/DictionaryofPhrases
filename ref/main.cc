5acb59d5079cb3293a0845242b69c7f790c26cc2
Riya Shrestha <rshrest2@stevens.edu>
2022-11-10 12:21:39 -0500

    created a file for the source code

---
+++ ref/main.cc
@@ -0,0 +1,12 @@
+/*Get words from a book without punc and pick the common phrases
+
+Count the # of books that has the phrases
+Count all instances of the times the phrase is recorded as long as there is only 1 changing variable
+
+Focus on one set of books!
+
+Picks books from the same genre for the same dict
+How do we deal with the proper nouns (Names)
+
+Encode all parametric phrases
+*/
